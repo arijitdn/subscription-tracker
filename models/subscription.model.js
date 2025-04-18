b41881b5ef4c6571212d2b62c1b035d7a37a86bf
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-04-18 23:25:16 +0530

    chore: update dependencies and add formatting scripts
    - Updated express to version 4.21.2 in package.json.
    - Added prettier as a dependency and configured it with a .prettierrc file.
    - Added a format script to package.json for code formatting.
    - Updated routes to use consistent quotation marks and formatting.
    - Created .prettierignore to exclude node_modules and dist directories.

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,93 +1,98 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
+const subscriptionSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Subscription name is required"],
-        trim: true,
-        minLength: 3,
-        maxLength: 50,
+      type: String,
+      required: [true, "Subscription name is required"],
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
     },
     price: {
-        type: Number,
-        required: [true, "Subscription price is required"],
-        min: 0,
+      type: Number,
+      required: [true, "Subscription price is required"],
+      min: 0,
     },
     currency: {
-        type: String,
-        required: [true, "Subscription currency is required"],
-        trim: true,
-        enum: ["USD", "EUR", "INR"],
-        default: "USD",
+      type: String,
+      required: [true, "Subscription currency is required"],
+      trim: true,
+      enum: ["USD", "EUR", "INR"],
+      default: "USD",
     },
     frequency: {
-        type: String,
-        required: [true, "Subscription frequency is required"],
-        trim: true,
-        enum: ["monthly", "yearly"],
-        default: "monthly",
+      type: String,
+      required: [true, "Subscription frequency is required"],
+      trim: true,
+      enum: ["monthly", "yearly"],
+      default: "monthly",
     },
     category: {
-        type: String,
-        required: [true, "Subscription category is required"],
-        trim: true,
-        enum: ["basic", "premium", "enterprise"],
+      type: String,
+      required: [true, "Subscription category is required"],
+      trim: true,
+      enum: ["basic", "premium", "enterprise"],
     },
     paymentMethod: {
-        type: String,
-        required: [true, "Payment method is required"],
-        trim: true,
-        enum: ["credit_card", "paypal", "UPI"],
+      type: String,
+      required: [true, "Payment method is required"],
+      trim: true,
+      enum: ["credit_card", "paypal", "UPI"],
     },
     status: {
-        type: String,
-        required: [true, "Subscription status is required"],
-        trim: true,
-        enum: ["active", "inactive"],
-        default: "active",
+      type: String,
+      required: [true, "Subscription status is required"],
+      trim: true,
+      enum: ["active", "inactive"],
+      default: "active",
     },
     startDate: {
-        type: Date,
-        required: [true, "Subscription start date is required"],
-        validate: {
-            validator:(value) => value <= new Date(),
-            message: "Start date cannot be in the future",
-        }
+      type: Date,
+      required: [true, "Subscription start date is required"],
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date cannot be in the future",
+      },
     },
     renewalDate: {
-        type: Date,
-        validate: {
-            validator: function (value) {
-                 value > this.startDate;
-            },
-            message: "Start date cannot be in the future",
-        }
+      type: Date,
+      validate: {
+        validator: function (value) {
+          value > this.startDate;
+        },
+        message: "Start date cannot be in the future",
+      },
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: [true, "User is required"],
-        index: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+      index: true,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-subscriptionSchema.pre('save', function (next) {
-    if(!this.renrewalDate){
-        const renewalPeriod = {
-            daily: 1,
-            weekly:7,
-            monthly:28,
-            yearly:365
-        };
-        this.renewalDate= new Date(this.startDate);
-        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriod[this.frequency]);
-    }
+subscriptionSchema.pre("save", function (next) {
+  if (!this.renrewalDate) {
+    const renewalPeriod = {
+      daily: 1,
+      weekly: 7,
+      monthly: 28,
+      yearly: 365,
+    };
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.renewalDate.getDate() + renewalPeriod[this.frequency]
+    );
+  }
 
-    if(this.renewalDate < this.Date){
-        this.status = "inactive";
-    }
+  if (this.renewalDate < this.Date) {
+    this.status = "inactive";
+  }
 
-    next();
+  next();
 });
 
 const Subscription = mongoose.model("Subscription", subscriptionSchema);
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,21 +1,37 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/',(req,res) => res.send({title:'Get all subscriptions'}));
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ title: "Get all subscriptions" })
+);
 
-subscriptionRouter.get('/:id',(req,res) => res.send({title:'Get subscriptions details'}));
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "Get subscriptions details" })
+);
 
-subscriptionRouter.post('/',(req,res) => res.send({title:'create subscriptions'}));
+subscriptionRouter.post("/", (req, res) =>
+  res.send({ title: "create subscriptions" })
+);
 
-subscriptionRouter.put('/',(req,res) => res.send({title:'update subscriptions'}));
+subscriptionRouter.put("/", (req, res) =>
+  res.send({ title: "update subscriptions" })
+);
 
-subscriptionRouter.delete('/:id',(req,res) => res.send({title:'delete subscriptions'}));
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ title: "delete subscriptions" })
+);
 
-subscriptionRouter.get('/:user:id',(req,res) => res.send({title:'Get all user subscriptions'}));
+subscriptionRouter.get("/:user:id", (req, res) =>
+  res.send({ title: "Get all user subscriptions" })
+);
 
-subscriptionRouter.put('/:id/cancel',(req,res) => res.send({title:'cancel all subscriptions'}));
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "cancel all subscriptions" })
+);
 
-subscriptionRouter.get('/upcoming-renewals',(req,res) => res.send({title:'Get all upcoming renewals'}));
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "Get all upcoming renewals" })
+);
 
 export default subscriptionRouter;
